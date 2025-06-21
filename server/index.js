10ecdd568dd216602653b74edaacc488ea43e3be
Stealth <47306082+StealthHydra179@users.noreply.github.com>
2025-06-20 22:16:19 -0400

    Logger setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const logger = require('./logger').logger;
+
+const $TAG = 'server_index';
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  logger.info(`$TAG Server listening on port ${port}`)
+})
